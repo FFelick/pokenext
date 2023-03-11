25b1f596b2dc896d7fc56564a601d341fdf346d8
ffelick <fernandofewp@hotmail.com>
2023-03-11 14:38:58 -0300

    api, constantes e requisição

--- pages/about.js
+++ pages/about.js
@@ -10,7 +10,9 @@ export default function About() {
                     <h1>Sobre o Projeto</h1>
                     <p>Esse projeto foi desenvolvido com o intuito de aplicar os conhecimentos React, Next e Javascript. Para que esse projeto fosse viável e acessível, utilizei <Link href="https://pokeapi.co/" target="_blank">PokeAPI</Link>, dessa forma ela contém dados como um backend, simplificando o projeto. Mas se é um fã de Pokemon, se sinta à vontade para relembrar dos seus preferidos e saber mais informações.</p>
                 </div>
+                <div className={styles.about3}>
                 <Image src="/images/charizard.png" width={300} height={200} alt="Charizard" />
+                </div>
             </div>
 
         </>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,38 @@
 
 import styles from '@/styles/Home.module.css'
 
+export async function getStaticProps() {
 
-export default function Home() {
+  const maxPokemons = 250
+  const api = 'https://pokeapi.co/api/v2/pokemon/'
+
+  //Requisição
+  const res = await fetch(`${api}/?limit=${maxPokemons}`)
+  const data = await res.json()
+
+  //add index pokemon
+  data.results.forEach((item, index) => {
+    item.id = index + 1
+  })
+
+  return {
+    props: {
+      pokemons: data.results,
+    },
+  }
+}
+
+export default function Home({ pokemons }) {
   return (
     <>
-    <div >
-     <h1>PokeNext</h1>
-     </div>
+      <div >
+        <h1>PokeNext</h1>
+        <ul>
+          {pokemons.map((pokemon) =>(
+            <li key={pokemon.id}>{pokemon.name}</li>
+          ))}
+        </ul>
+      </div>
     </>
   )
 }
