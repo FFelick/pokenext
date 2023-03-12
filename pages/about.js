7687a92e41e4bd15b2e54b0dfa8d151ec9a656fc
ffelick <fernandofewp@hotmail.com>
2023-03-12 00:17:11 -0300

    finalização

--- pages/about.js
+++ pages/about.js
@@ -8,7 +8,7 @@ export default function About() {
             <div className={styles.about}>
                 <div className={styles.about2}>
                     <h1>Sobre o Projeto</h1>
-                    <p>Esse projeto foi desenvolvido com o intuito de aplicar os conhecimentos React, Next e Javascript. Para que esse projeto fosse viável e acessível, utilizei <Link href="https://pokeapi.co/" target="_blank">PokeAPI</Link>, dessa forma ela contém dados como um backend, simplificando o projeto. Mas se é um fã de Pokemon, se sinta à vontade para relembrar dos seus preferidos e saber mais detalhes.</p>
+                    <p>Esse projeto foi desenvolvido com o intuito de aplicar os conhecimentos React, Next e Javascript. Para que esse projeto fosse viável e acessível, utilizei <Link href="https://pokeapi.co/" target="_blank">PokeAPI</Link>, dessa forma ela contém dados como um backend, simplificando o projeto. Se é um fã de Pokemon, se sinta à vontade para relembrar dos seus preferidos e saber mais detalhes.</p>
                 </div>
                 <div className={styles.about3}>
                 <Image src="/images/charizard.png" width={300} height={200} alt="Charizard" />
--- pages/pokemon/[pokemonid].js
+++ pages/pokemon/[pokemonid].js
@@ -1,8 +1,12 @@
-export const getStaticPaths = async() => {
+import Image from "next/image"
+
+import styles from "@/styles/Pokemon.module.css"
+
+export const getStaticPaths = async () => {
 
     const maxPokemons = 300
     const api = 'https://pokeapi.co/api/v2/pokemon/'
-  
+
     //Requisição
     const res = await fetch(`${api}/?limit=${maxPokemons}`)
     const data = await res.json()
@@ -10,7 +14,7 @@ export const getStaticPaths = async() => {
     // paramentos
     const paths = data.results.map((pokemon, index) => {
         return {
-            params: {pokemonid: (index + 1).toString() },
+            params: { pokemonid: (index + 1).toString() },
         }
     })
 
@@ -20,7 +24,7 @@ export const getStaticPaths = async() => {
     }
 }
 
-export const getStaticProps = async(context) => {
+export const getStaticProps = async (context) => {
 
     const id = context.params.pokemonid
 
@@ -32,10 +36,40 @@ export const getStaticProps = async(context) => {
     }
 }
 
-export default function Pokemon({pokemon}) {
+export default function Pokemon({ pokemon }) {
     return (
         <>
-            <p>{pokemon.name}</p>
+            <div className={styles.pokemon_container}>
+                <h1 className={styles.title}>{pokemon.name}</h1>
+                <Image
+                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+                    width="200"
+                    height="200"
+                    alt={pokemon.name}
+                />
+                <div>
+                    <h3>Número:</h3>
+                    <p>#{pokemon.id}</p>
+                </div>
+                <div>
+                    <h3>Tipo:</h3>
+                    <div className={styles.types_container}>
+                        {pokemon.types.map((item, index) => (
+                            <span key={index} className={`${styles.type} ${styles['type_' + item.type.name]}`}>{item.type.name}</span>
+                        ))}
+                    </div>
+                    <div className={styles.data_container}>
+                        <div className={styles.data_height}>
+                            <h4>Altura:</h4>
+                            <p>{pokemon.height * 10} cm</p>
+                        </div>
+                        <div className={styles.data_weight}>
+                            <h4>Peso:</h4>
+                            <p>{pokemon.weight / 10} kg</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
